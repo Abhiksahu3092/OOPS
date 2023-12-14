1e4fd6c74d808ddb56379b3633f7252d359e8056
Abhik Sahu <abhiksahu2003@gmail.com>
2023-12-14 13:28:01 +0530

    simply nothing !!!!!!

--- new/9.cpp
+++ new/9.cpp
@@ -2,3 +2,14 @@
 using namespace std;
 
 /*friend function in oops -> It is used to access private member of a class even though this function is not itself a member of this class*/
+
+class myclass{
+    int data;
+    public:
+    int set_data(int i){
+        data=i;
+    }
+    int get_data(){
+        return data;
+    }
+};
