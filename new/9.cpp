ede0e890e21da67f617c28f6ac332e8992086e29
Abhik Sahu <abhiksahu2003@gmail.com>
2023-12-14 17:24:39 +0530

    friend function

--- new/9.cpp
+++ new/9.cpp
@@ -4,12 +4,27 @@ using namespace std;
 /*friend function in oops -> It is used to access private member of a class even though this function is not itself a member of this class*/
 
 class myclass{
-    int data;
+    int data1;
+    int data2;
     public:
-    int set_data(int i){
-        data=i;
+    int set_data(int i,int j){
+        data1=i;
+        data2=j;
     }
     int get_data(){
-        return data;
+        return data1;
     }
+
+    friend int mul(myclass obj);//here a friend function is defined which is not a member function of class myclass
 };
+
+int mul(myclass obj){
+    int ans=obj.data1*obj.data2;
+    return ans;
+}
+
+int main(){
+    myclass obj;
+    obj.set_data(10,2);
+    cout<<mul(obj)<<endl;
+}
